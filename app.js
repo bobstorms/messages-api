6998ad8ae7fecb922acff1ab1b52a42f2a4f942e
Bob Storms <bob.storms@hotmail.be>
2021-03-17 22:18:35 +0100

    Changed PORT

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 const routerMessages = require("./routers/api/v1/messages");
 const pug = require('pug');
 
